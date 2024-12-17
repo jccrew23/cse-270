71b0da372b865b611a9a97c722d158014e1a9ea0
cur24001@byui.edu <cur24001@byui.edu>
2024-12-17 01:15:55 -0500

    retry

--- teton/1.5/scripts/join.js
+++
@@ -1,8 +0,0 @@
-let url = new URL(window.location);
-let params  = url.searchParams;
-let form = document.querySelector("form")
-
-for (const p of params) {            
-  form.innerHTML+=`\n<input type="hidden" name="${p[0]}" value="${p[1]}">`
-}
-
